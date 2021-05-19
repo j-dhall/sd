d3824c7581b4917c1d6c47c9285f748f9e6e3503
j-dhall <74283389+j-dhall@users.noreply.github.com>
2021-05-19 11:59:25 +0530

    ThymeLeaf - added a ModelAndView returnable endpoint, and ThymeLeaf enabled HTML view

--- java/spring-boot-demo/demo/src/main/java/com/sd/spring/boot/mvc/demo/SimpleController.java
+++ java/spring-boot-demo/demo/src/main/java/com/sd/spring/boot/mvc/demo/SimpleController.java
@@ -1,12 +1,20 @@
 package com.sd.spring.boot.mvc.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SimpleController {
+	@Autowired
+	private BookRepository bookRepository;
+	
 	@Value("${spring.application.name}")
 	String appName;
 
@@ -14,5 +22,13 @@ public class SimpleController {
 	public String homePage(Model model) {
 		model.addAttribute("appName", appName);
 		return "home";// "/templates/home.html"
+	}
+	
+	@GetMapping("/all")
+	public ModelAndView findAll() {
+		Iterable<Book> books = bookRepository.findAll();
+		Map<String, Object> attributes = new HashMap<>();
+		attributes.put("books", books);
+		return new ModelAndView("home", attributes);
 	}
 }
