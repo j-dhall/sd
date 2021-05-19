26c0f82f45c12c220f92b5f36f9e38c91dd2968c
j-dhall <74283389+j-dhall@users.noreply.github.com>
2021-05-19 12:02:37 +0530

    media-app: added a POST endpoint returning ModelAndView

--- java/media-app/src/main/java/io/educative/mediaapp/PlayListController.java
+++ java/media-app/src/main/java/io/educative/mediaapp/PlayListController.java
@@ -1,13 +1,18 @@
 package io.educative.mediaapp;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/playlist")
@@ -18,8 +23,16 @@ public class PlayListController {
 		return "index";
 	}
 	
-	@GetMapping("/rest")
+	@GetMapping("/all")
 	public @ResponseBody PlayList getAllPlaylists() {
 		return new PlayList();
+	}
+	
+	@PostMapping("/new")
+	public ModelAndView createPlaylist(@RequestBody PlayList playlist) {
+		Map<String, Object> attributes = new HashMap<>();
+		attributes.put("playlists", playlist);
+		
+		return new ModelAndView("index", attributes);
 	}
 }
