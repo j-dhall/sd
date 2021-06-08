f6610749545c89cb06b3fdb4b8a02dd742bcb8b7
j-dhall <74283389+j-dhall@users.noreply.github.com>
2021-06-08 15:47:48 +0530

    enabled logging

--- java/catalog/src/main/java/edu/ds/ms/retail/catalog/controller/CategoryController.java
+++ java/catalog/src/main/java/edu/ds/ms/retail/catalog/controller/CategoryController.java
@@ -9,15 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import edu.ds.ms.retail.catalog.entity.Category;
 import edu.ds.ms.retail.catalog.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/api")
-public class CategoriesController {
+@RequestMapping("/categories")
+@Slf4j
+public class CategoryController {
 	@Autowired
 	CategoryService categoryService;
 	
-	@GetMapping(value = "/category")
+	@GetMapping
 	public List<Category> getAllCategories() {
+		//logging demo using lombak slf4j (defaults to using Logback implementation)
+		log.trace("A TRACE Message.");
+		log.debug("A DEBUG Message.");
+		log.info("A INFO Message.");
+		log.warn("A WARN Message.");
+		log.error("A ERROR Message.");
+		
 		List<Category> categories = categoryService.getAllCategories(); 
 		return categories;
 	}
