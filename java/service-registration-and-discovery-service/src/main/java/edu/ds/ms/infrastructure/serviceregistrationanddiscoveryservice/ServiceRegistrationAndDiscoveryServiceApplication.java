34b31f50c4c2f38c8b6142d88fd5c44b5db467b8
j-dhall <74283389+j-dhall@users.noreply.github.com>
2021-06-14 16:27:25 +0530

    Netflix Eureka Service Discovery

---
+++ java/service-registration-and-discovery-service/src/main/java/edu/ds/ms/infrastructure/serviceregistrationanddiscoveryservice/ServiceRegistrationAndDiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package edu.ds.ms.infrastructure.serviceregistrationanddiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistrationAndDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistrationAndDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ java/service-registration-and-discovery-service/src/test/java/edu/ds/ms/infrastructure/serviceregistrationanddiscoveryservice/ServiceRegistrationAndDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ds.ms.infrastructure.serviceregistrationanddiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistrationAndDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
