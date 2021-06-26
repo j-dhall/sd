6b542c85db4f3ab5544daa3bef9e21d1276cc762
j-dhall <74283389+j-dhall@users.noreply.github.com>
2021-06-26 18:37:39 +0530

    enabled Gradle in java console application

--- java/HelloJavaGradle/src/main/java/edu/java/gradle/HelloJavaGradle.java
+++ java/HelloJavaGradle/src/main/java/edu/java/gradle/HelloJavaGradle.java
@@ -4,7 +4,7 @@ public class HelloJavaGradle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World, Gradle!!");
+		System.out.println("Hello World Java Gradle!!");
 	}
 
 }
