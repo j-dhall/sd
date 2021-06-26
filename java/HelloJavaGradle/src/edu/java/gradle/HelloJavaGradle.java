691a97b8fe5d33851dad77b423888455c235b441
j-dhall <74283389+j-dhall@users.noreply.github.com>
2021-06-26 18:07:46 +0530

    java console application, going to enable gradle

---
+++ java/HelloJavaGradle/src/edu/java/gradle/HelloJavaGradle.java
@@ -0,0 +1,10 @@
+package edu.java.gradle;
+
+public class HelloJavaGradle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World, Gradle!!");
+	}
+
+}
