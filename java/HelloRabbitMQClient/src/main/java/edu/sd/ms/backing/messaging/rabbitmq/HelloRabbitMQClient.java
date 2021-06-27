94e8763a2000f8987cd995168e9b829a6f69d0f5
j-dhall <74283389+j-dhall@users.noreply.github.com>
2021-06-27 15:32:41 +0530

    RabbitMQ Producer-Consumer Example

--- java/HelloRabbitMQClient/src/main/java/edu/sd/ms/backing/messaging/rabbitmq/HelloRabbitMQClient.java
+++ java/HelloRabbitMQClient/src/main/java/edu/sd/ms/backing/messaging/rabbitmq/HelloRabbitMQClient.java
@@ -1,10 +1,42 @@
 package edu.sd.ms.backing.messaging.rabbitmq;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
 public class HelloRabbitMQClient {
+	
+	private final static String QUEUE_NAME = "hello";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello RabbitMQClient");
+		
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		try {
+			Connection conn = factory.newConnection();
+			Channel channel = conn.createChannel();
+			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+			System.out.println("Waiting for messages. To exit, press CTRL+C");
+			
+			DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+				String message = new String(delivery.getBody(), "UTF-8");
+				System.out.println("Received: '" + message + "'.");
+			};
+			channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {});
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- java/HelloRabbitMQServer/src/main/java/edu/sd/ms/backing/messaging/rabbitmq/HelloRabbitMQServer.java
+++ java/HelloRabbitMQServer/src/main/java/edu/sd/ms/backing/messaging/rabbitmq/HelloRabbitMQServer.java
@@ -1,10 +1,38 @@
 package edu.sd.ms.backing.messaging.rabbitmq;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
 public class HelloRabbitMQServer {
+	
+	private final static String QUEUE_NAME = "hello";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello RabbitMQServer");
+
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		try {
+			Connection conn = factory.newConnection();
+			Channel channel = conn.createChannel();
+			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+			String message = "Hello Message";
+			channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+			System.out.println("Sent: '" + message + "'.");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 }
